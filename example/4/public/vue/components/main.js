54c938a5873e156077039353193c000a447d24bc
Lilleman <lilleman@zmartbag.com>
2020-02-03 10:11:00 +0100

    Prove in example 4 that both ssr and client side works

--- example/4/public/vue/components/main.js
+++ example/4/public/vue/components/main.js
@@ -1,6 +1,28 @@
 export default async function (options) {
 	const { store } = options;
 	return {
+		computed: {
+			foo() {
+				return this.$store.state.foo;
+			}
+		},
+		mounted() {
+			const foos = [
+				'berit',
+				'bosse',
+				'bengt',
+				'lina',
+				'ludmilla-ludwig',
+			];
+			setInterval(() => {
+				const randomNumber = Math.floor(Math.random()*foos.length);
+				this.$store.commit('setFoo', foos[randomNumber]);
+			}, 1000);
+		},
+		serverPrefetch() {
+			console.log('wat');
+			this.$store.commit('setFoo', 'bosse');
+		},
 		store,
 		template: `<div id="vue-main">
 			<ul>
@@ -8,6 +30,7 @@ export default async function (options) {
 				<li><router-link to="/foo">Foo</router-link></li>
 			</ul>
 			<router-view></router-view>
+			<p>{{ foo }}</p>
 		</div>`
 	};
 }
